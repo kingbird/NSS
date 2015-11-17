683cad8351c15a7e697e2472aa97f7c4a72250e0
kingbird <4739345@qq.com>
2015-11-17 22:58:43 +0800

    modify

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ var http = require('http'),
 
 var NSS = new http.Server;
 
-function getZipType(zipTypes) {
-	var zipType;
+
+NSS.getZipType = function(zipTypes) {
+	var zipTypes = zipTypes.toString();
 	if(zipTypes.indexOf('gzip') != -1) {
-		zipType = 'gzip';
-		return zipType;
+		return 'gzip';
 	} else if (zipTypes.indexOf('deflate') != -1) {
-		zipType = 'deflate';
+		return 'deflate';
+	} else {
+		return;
 	}
 }
 
 NSS.on('request', function(req, res) {
 	var reqURL = url.parse(req.url),
-		filepath,
-		zipType = getZipType(req.headers['accept-encoding']);
+		filepath;
 
 	if(!reqURL.query && !reqURL.hash && reqURL.path.toString().indexOf('.') == -1 && reqURL.path.slice(-1) != '/') {
 		filepath = config.ROOT_DIR + reqURL.pathname + '/';
@@ -32,16 +33,7 @@ NSS.on('request', function(req, res) {
 	}
 
 	filepath = decodeURIComponent(filepath);
-
-	var fileType = filepath.match(/\.[^\.]+$/g).toString().slice(1),
-		mime = mineTypes[fileType] || 'octet-stream';
-
 	fs.stat(filepath, function(err, stats) {
-
-		var mTime = stats.mtime.toUTCString();
-		console.log(req.headers['if-modified-since']);
-		console.log(mTime);
-		console.log(req.headers['if-modified-since'] == mTime);
 		if(err) {
 			res.writeHead(404, {
 				'Server': 'NSS',
@@ -49,18 +41,18 @@ NSS.on('request', function(req, res) {
 			});
 			res.write('404 not found');
 			res.end();
-		} else if (req.headers['if-modified-since'] && req.headers['if-modified-since'] == mTime) {
-			console.log();
-			res.statusCode = 304;
-			res.setHeader('Server', 'NSS');
-			res.setHeader('Content-Encoding', zipType);
-			res.setHeader('Content-Type', mime);	
-			res.setHeader('Last-Modified', mTime);
-			res.setHeader('Cache-Control', 'max-age=' + config.MAX_AGE);
-			var bodyData = fs.createReadStream(filepath);
-			bodyData.pipe(zlib.createGzip()).pipe(res);
 		} else {
-			res.statusCode = 200;
+			var fileType = filepath.match(/\.[^\.]+$/g).toString().slice(1),
+				mime = mineTypes[fileType] || 'octet-stream',
+				zipType = NSS.getZipType(req.headers['accept-encoding']);
+				mTime = stats.mtime.toUTCString();
+			if (req.headers['if-modified-since'] && req.headers['if-modified-since'] == mTime) {
+				res.statusCode = 304;
+			} else {
+				res.statusCode = 200;
+			}
+			console.log(NSS.getZipType);
+			console.log(this.getZipType);
 			res.setHeader('Server', 'NSS');
 			res.setHeader('Content-Encoding', zipType);
 			res.setHeader('Content-Type', mime);	
